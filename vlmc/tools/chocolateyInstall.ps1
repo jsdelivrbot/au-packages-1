2ab335f006d20ba6780e843fb87b3165676ab660
Christoffer <netravnen@gmail.com>
2018-07-05 20:56:59 +0200

    Added package vlmc

---
+++ vlmc/tools/chocolateyInstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName            = 'vlmc'
+  fileType               = 'exe'
+  url                    = 'http://ftp.videolan.org/pub/testing/vlmc/vlmc-0.2.0-win32.exe'
+  checksum               = '63599EBA59D3A5A97904C39286C22C2C3B9FDF343B149B79C2A840F51D4DB5F0'
+  checksumType           = 'sha256'
+  silentArgs             = '/VERYSILENT'
+  validExitCodes         = @(0)
+  softwareName           = 'vlmc*'
+}
+Install-ChocolateyPackage @packageArgs
+
+$packageName = $packageArgs.packageName
+$installLocation = Get-AppInstallLocation $packageName
+if (!$installLocation)  { Write-Warning "Can't find $packageName install location"; return }
+Write-Host "$packageName installed to '$installLocation'"
+
+Register-Application "$installLocation\$packageName.exe"
+Write-Host "$packageName registered as $packageName"
---
+++ vlmc/update.ps1
@@ -0,0 +1,41 @@
+import-module au
+. $PSScriptRoot\..\_scripts\all.ps1
+
+$releases = 'http://ftp.videolan.org/pub/testing/vlmc/'
+
+function global:au_SearchReplace {
+   @{
+        ".\tools\chocolateyInstall.ps1" = @{
+            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*packageName\s*=\s*)('.*')"  = "`$1'$($Latest.PackageName)'"
+            "(?i)(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)*'"
+            "(?i)(^\s*fileType\s*=\s*)('.*')"     = "`$1'$($Latest.FileType)'"
+        }
+
+        "$($Latest.PackageName).nuspec" = @{
+            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
+        }
+
+        ".\legal\VERIFICATION.txt" = @{
+          "(?i)(\s+x32:).*"            = "`${1} $($Latest.URL32)"
+          "(?i)(checksum32:).*"        = "`${1} $($Latest.Checksum32)"
+        }
+    }
+}
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $re    = '\.exe$'
+    $url   = $download_page.links | ? href -match $re | select -Last 1 -expand href
+    $version  = $url -split '[-]|.exe' | select -First 1 -Skip 1
+    $url = "http://ftp.videolan.org/pub/testing/vlmc/$url"
+    @{
+        URL32     = $url
+        Version   = $version
+    }
+}
+
+update -ChecksumFor 32
