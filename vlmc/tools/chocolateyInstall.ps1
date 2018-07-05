5f14b66aab1db80e5c79a85097e94cdfa3b0e167
Christoffer <netravnen@gmail.com>
2018-07-05 23:20:22 +0200

    VLMC: fix silentarg for package

--- vlmc/tools/chocolateyInstall.ps1
+++ vlmc/tools/chocolateyInstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   url                    = 'http://ftp.videolan.org/pub/testing/vlmc/vlmc-0.2.0-win32.exe'
   checksum               = '63599EBA59D3A5A97904C39286C22C2C3B9FDF343B149B79C2A840F51D4DB5F0'
   checksumType           = 'sha256'
-  silentArgs             = '/VERYSILENT'
+  silentArgs             = '/S'
   validExitCodes         = @(0)
   softwareName           = 'vlmc*'
 }
